696e5e2fb9bf3ff7e5c1dca158eac9fb67c6bf51
geezah <emre.gezer@hotmail.de>
2024-11-30 18:05:39 +0100

    adjust default path for `parent_dir`

--- tasks.py
+++ tasks.py
@@ -12,8 +12,9 @@ def _download_synapse(syn_client, syn_id, parent_dir):
     output_dir = Path(parent_dir) / syn_id
     synapseutils.syncFromSynapse(syn_client, entity=syn_id, path=output_dir)
 
+
 @task
-def download(c: Context, synapse_id : str, parent_dir: str = "data/raw"):
+def download(c: Context, synapse_id: str, parent_dir: str = "local_data/raw"):
     syn = synapseclient.Synapse()
     syn.login(
         email=environ.get("SYNAPSE_EMAIL"),
