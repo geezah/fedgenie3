06d575547e34bd0556ab98bd6a7f2f1bf7b74fb0
geezah <emre.gezer@hotmail.de>
2024-12-08 16:39:29 +0100

    Refactor run function to accept GRNDataset and kwargs, update centralized run script to match new signature

--- fedgenie3/genie3/run.py
+++ fedgenie3/genie3/run.py
@@ -1,35 +1,33 @@
-from pathlib import Path
+from typing import Any, Dict
 
-from fedgenie3.data.dataset import load_dream_five
+from fedgenie3.data.dataset import GRNDataset
+from fedgenie3.genie3.configs import get_regressor_init_params
 from fedgenie3.genie3.eval import evaluate_ranking
 from fedgenie3.genie3.modeling import GENIE3
-from fedgenie3.genie3.configs import get_regressor_init_params
 
 
-def run(root: Path, network_id: int, dev_run: bool = False):
-    print(
-        f"Running (non-federated) GRN inference for network {network_id} in {root}"
+def run(dataset: GRNDataset, **kwargs: Dict[str, Any]):
+    regressor_type = kwargs.get("regressor_type", "LGBM")
+    regressor_init_params = kwargs.get(
+        "regressor_init_params", get_regressor_init_params(regressor_type)
     )
-    grn_dataset = load_dream_five(root, network_id)
-    regressor_type = "LGBM"
-    regressor_init_params = get_regressor_init_params(regressor_type)
     genie3 = GENIE3(
         regressor_type=regressor_type,
         regressor_init_params=regressor_init_params,
     )
     importance_matrix = genie3.calculate_importances(
-        grn_dataset.gene_expressions.values,
-        grn_dataset.metadata.transcription_factor_indices.to_list(),
-        dev_run=dev_run
+        dataset.gene_expressions.values,
+        dataset.metadata.transcription_factor_indices.to_list(),
+        dev_run=kwargs.get("dev_run", False),
     )
     gene_ranking_with_indices = GENIE3.rank_genes_by_importance(
-        importance_matrix, grn_dataset.metadata.transcription_factor_indices
+        importance_matrix, dataset.metadata.transcription_factor_indices
     )
     gene_ranking_with_names = GENIE3.map_indices_to_gene_names(
-        gene_ranking_with_indices, grn_dataset.metadata.gene_names_to_indices
+        gene_ranking_with_indices, dataset.metadata.gene_names_to_indices
     )
     results = evaluate_ranking(
-        gene_ranking_with_names, grn_dataset.reference_network
+        gene_ranking_with_names, dataset.reference_network
     )
     print(results)
     return results
--- scripts/run_centralized.py
+++ scripts/run_centralized.py
@@ -1,6 +1,10 @@
-from typer import Typer
-from fedgenie3.genie3.run import run
 from pathlib import Path
+
+from typer import Typer
+
+from fedgenie3.data.dataset import load_dream_five
+from fedgenie3.genie3.configs import get_regressor_init_params
+from fedgenie3.genie3.run import run
 
 app = Typer(pretty_exceptions_show_locals=False)
 
@@ -9,9 +13,20 @@ app = Typer(pretty_exceptions_show_locals=False)
 def main(
     root: Path = Path("local_data/processed/dream_five"),
     network_id: int = 1,
-    dev_run: bool = True,
+    dev_run: bool = False,
 ):
-    run(root, network_id, dev_run)
+    print(
+        f"Running (non-federated) GRN inference for network {network_id} in {root}"
+    )
+    grn_dataset = load_dream_five(root, network_id)
+    regressor_type = "LGBM"
+    regressor_init_params = get_regressor_init_params(regressor_type)
+    run(
+        grn_dataset,
+        regressor_type=regressor_type,
+        regressor_init_params=regressor_init_params,
+        dev_run=dev_run,
+    )
 
 
 if __name__ == "__main__":
