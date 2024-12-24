6b441c5d20c0dc71f892fcf774d748710b5bae6e
geezah <emre.gezer@hotmail.de>
2024-12-24 02:13:31 +0100

    Enhance module exports by adding functions to `__all__` in `__init__.py`

--- genie3/modeling/__init__.py
+++ genie3/modeling/__init__.py
@@ -1,5 +1,13 @@
-from .genie3 import GENIE3
+from .genie3 import (
+    GENIE3,
+    calculate_importances,
+    partition_data,
+    rank_genes_by_importance,
+)
 
 __all__ = [
     "GENIE3",
+    "calculate_importances",
+    "rank_genes_by_importance",
+    "partition_data",
 ]
