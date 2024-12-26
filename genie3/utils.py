9ad0d9bf8325c062dbdcbae372a8dbd06b169982
geezah <emre.gezer@hotmail.de>
2024-12-26 03:16:56 +0100

    Refactor result saving functions to improve modularity

--- genie3/utils.py
+++ genie3/utils.py
@@ -1,13 +1,94 @@
-import json
 from pathlib import Path
 
 import pandas as pd
+import yaml
 from matplotlib.figure import Figure
 
 from genie3.config import GENIE3Config
 
 
-def save_results_inference_only(
+def write_config(config: GENIE3Config, output_dir: Path) -> None:
+    """
+    Write the configuration to a YAML file.
+
+    Args:
+        config (GENIE3Config): The configuration object for the GENIE3 model.
+        output_dir (Path): The directory where the configuration will be saved.
+
+    Returns:
+        None
+    """
+    output_dir.mkdir(parents=True, exist_ok=True)
+    with open(output_dir / "config.yaml", "w", encoding="utf-8") as f:
+        # Convert the GENIE3Config object to a dictionary with paths casted to strings and dump it as JSON
+        config.data.gene_expressions_path = str(
+            config.data.gene_expressions_path.resolve()
+        )
+        if config.data.transcription_factors_path is not None:
+            config.data.transcription_factors_path = str(
+                config.data.transcription_factors_path.resolve()
+            )
+        if config.data.reference_network_path is not None:
+            config.data.reference_network_path = str(
+                config.data.reference_network_path.resolve()
+            )
+
+        config = config.model_dump()
+        yaml.safe_dump(config, f)
+
+
+def write_network(network: pd.DataFrame, output_path: Path) -> None:
+    """
+    Write a network to a file.
+
+    Args:
+        network (pd.DataFrame): The network to save.
+        output_path (Path): The path to save the network to.
+
+    Returns:
+        None
+    """
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    network.to_csv(output_path, index=False)
+
+
+def write_metrics(auroc: float, auprc: float, output_dir: Path) -> None:
+    """
+    Write the AUROC and AUPRC metrics to a CSV file.
+
+    Args:
+        auroc (float): The Area Under the Receiver Operating Characteristic curve score.
+        auprc (float): The Area Under the Precision-Recall curve score.
+        output_dir (Path): The directory where the metrics will be saved.
+
+    Returns:
+        None
+    """
+    output_dir.mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(
+        {
+            "metric": ["AUROC", "AUPRC"],
+            "score": [auroc, auprc],
+        },
+    ).to_csv(output_dir / "metrics.csv", index=False)
+
+
+def write_plot(plot: Figure, output_path: Path) -> None:
+    """
+    Write a plot to a file.
+
+    Args:
+        plot (Figure): The plot to save.
+        output_path (Path): The path to save the plot to.
+
+    Returns:
+        None
+    """
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    plot.savefig(output_path)
+
+
+def write_results_inference_only(
     config: GENIE3Config,
     predicted_network: pd.DataFrame,
     output_dir: Path = Path("results"),
@@ -25,15 +106,12 @@ def save_results_inference_only(
     """
     output_dir.mkdir(parents=True, exist_ok=True)
     # Dump the model configuration
-    with open(output_dir / "config.json", "w", encoding="utf-8") as f:
-        # Convert the GENIE3Config object to a dictionary with paths casted to strings and dump it as JSON
-        config = json.loads(config.model_dump_json())
-        json.dump(config, f, indent=4)
+    write_config(config, output_dir)
     # Dump the predicted network
-    predicted_network.to_csv(output_dir / "predicted_network.csv", index=False)
+    write_network(predicted_network, output_dir / "predicted_network.csv")
 
 
-def save_results_all(
+def write_results_full_pipeline(
     config: GENIE3Config,
     auroc: float,
     auprc: float,
@@ -59,18 +137,14 @@ def save_results_all(
     Returns:
         None
     """
-    save_results_inference_only(config, predicted_network, output_dir)
+    write_config(config, output_dir)
     # Dump the metrics
-    pd.DataFrame(
-        {
-            "metric": ["AUROC", "AUPRC"],
-            "score": [auroc, auprc],
-        },
-    ).to_csv(output_dir / "metrics.csv", index=False)
+    write_metrics(auroc, auprc, output_dir)
     # Dump the predicted and reference networks
-    reference_network.to_csv(output_dir / "reference_network.csv", index=False)
+    write_network(predicted_network, output_dir / "predicted_network.csv")
+    write_network(reference_network, output_dir / "reference_network.csv")
     # Dump the plots
-    roc_curve_plot.savefig(output_dir / "roc_curve.png")
-    precision_recall_curve_plot.savefig(
-        output_dir / "precision_recall_curve.png"
+    write_plot(roc_curve_plot, output_dir / "roc_curve.png")
+    write_plot(
+        precision_recall_curve_plot, output_dir / "precision_recall_curve.png"
     )
