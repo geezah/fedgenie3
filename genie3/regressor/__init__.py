4ac86ebdecb6f677ea9a37bf9f91fb8b843e8a0d
geezah <emre.gezer@hotmail.de>
2024-12-26 21:39:02 +0100

    Added XGBoost

--- genie3/regressor/__init__.py
+++ genie3/regressor/__init__.py
@@ -1,13 +1,22 @@
-from .extratrees import ExtraTreesRegressor
-from .lightgbm import LGBMRegressor
-from .randomforest import RandomForestRegressor
+from .extratrees import DefaultExtraTreesConfiguration, ExtraTreesRegressor
+from .lightgbm import DefaultLightGBMConfiguration, LGBMRegressor
+from .randomforest import (
+    DefaultRandomForestConfiguration,
+    RandomForestRegressor,
+)
+from .xgboost import DefaultXGBoostConfiguration, XGBRegressor
 
 RegressorFactory = {
     "RandomForestRegressor": RandomForestRegressor,
     "ExtraTreesRegressor": ExtraTreesRegressor,
     "LGBMRegressor": LGBMRegressor,
+    "XGBRegressor": XGBRegressor,
+}
+ConfigurationFactory = {
+    "RandomForestRegressor": DefaultRandomForestConfiguration,
+    "ExtraTreesRegressor": DefaultExtraTreesConfiguration,
+    "LGBMRegressor": DefaultLightGBMConfiguration,
+    "XGBRegressor": DefaultXGBoostConfiguration,
 }
 
-__all__ = [
-    "RegressorFactory",
-]
+__all__ = ["RegressorFactory", "ConfigurationFactory"]
