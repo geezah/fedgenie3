f7db4249cd7b9bc127d3cc3468935f15a44ea693
geezah <emre.gezer@hotmail.de>
2024-12-26 02:17:07 +0100

    Add default regressor configurations, remove xgboost

--- genie3/modeling/regressor.py
+++ genie3/modeling/regressor.py
@@ -9,9 +9,35 @@ from sklearn.ensemble import (
     RandomForestRegressor as _RandomForestRegressor,
 )
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor as _XGBRegressor
 
-RegressorName = Literal["RF", "ET", "LGBM", "XGB"]
+RegressorName = Literal["RF", "ET", "LGBM"]
+
+DefaultRandomForestConfiguration = {
+    "init_params": {
+        "n_estimators": 100,
+        "max_depth": 4,
+        "n_jobs": -1,
+        "random_state": 42,
+        "max_features": 0.8,
+    },
+    "fit_params": {},
+}
+
+DefaultExtraTreesConfiguration = DefaultRandomForestConfiguration
+DefaultLGBMConfiguration = {
+    "init_params": {
+        "n_estimators": 1000,
+        "learning_rate": 0.01,
+        "max_depth": 3,
+        "n_iter_no_change": 25,
+        "random_state": 42,
+        "importance_type": "gain",
+        "subsample": 0.8,
+        "colsample_bytree": 0.8,
+        "verbose": -1,
+    },
+    "fit_params": {},
+}
 
 
 class RegressorProtocol(Protocol):
@@ -36,10 +62,18 @@ class RandomForestRegressor(RegressorProtocol):
     def __init__(self, **init_params: Dict[str, Any]):
         self.regressor: _RandomForestRegressor = _RandomForestRegressor(
             **init_params
+            if init_params
+            else DefaultRandomForestConfiguration["init_params"]
         )
 
     def fit(self, X: NDArray, y: NDArray, **fit_params: Dict[str, Any]) -> Any:
-        self.regressor.fit(X, y, **fit_params)
+        self.regressor.fit(
+            X,
+            y,
+            **fit_params
+            if fit_params
+            else DefaultRandomForestConfiguration["fit_params"],
+        )
         self.feature_importances = self.regressor.feature_importances_
 
 
@@ -47,16 +81,28 @@ class ExtraTreesRegressor(RegressorProtocol):
     def __init__(self, **init_params: Dict[str, Any]):
         self.regressor: _ExtraTreesRegressor = _ExtraTreesRegressor(
             **init_params
+            if init_params
+            else DefaultExtraTreesConfiguration["init_params"]
         )
 
     def fit(self, X: NDArray, y: NDArray, **fit_params: Dict[str, Any]) -> Any:
-        self.regressor.fit(X, y, **fit_params)
+        self.regressor.fit(
+            X,
+            y,
+            **fit_params
+            if fit_params
+            else DefaultExtraTreesConfiguration["fit_params"],
+        )
         self.feature_importances = self.regressor.feature_importances_
 
 
 class LGBMRegressor(RegressorProtocol):
     def __init__(self, **init_params: Dict[str, Any]):
-        self.regressor = _LGBMRegressor(**init_params)
+        self.regressor: _LGBMRegressor = _LGBMRegressor(
+            **init_params
+            if init_params
+            else DefaultLGBMConfiguration["init_params"]
+        )
 
     def fit(self, X: NDArray, y: NDArray, **fit_params: Dict[str, Any]) -> Any:
         # In the case that early_stopping_rounds is provided, we need to split the data into training and validation sets.
@@ -64,27 +110,12 @@ class LGBMRegressor(RegressorProtocol):
             X, y, test_size=0.1, random_state=42
         )
         regressor = self.regressor.fit(
-            X_train, y_train, eval_set=[(X_val, y_val)], **fit_params
-        )
-        importances = regressor.feature_importances_
-        normalized_importances = importances / importances.sum()
-        self.feature_importances = normalized_importances
-
-
-class XGBRegressor(RegressorProtocol):
-    def __init__(self, **init_params: Dict[str, Any]):
-        self.regressor = _XGBRegressor(**init_params)
-
-    def fit(self, X: NDArray, y: NDArray, **fit_params: Dict[str, Any]) -> Any:
-        test_size = fit_params.get("test_size", 0.1)
-        random_state = fit_params.get("random_state", 42)
-
-        # In the case that early_stopping_rounds is provided, we need to split the data into training and validation sets.
-        X_train, X_val, y_train, y_val = train_test_split(
-            X, y, test_size=test_size, random_state=random_state
-        )
-        regressor = self.regressor.fit(
-            X_train, y_train, eval_set=[(X_val, y_val)], **fit_params
+            X_train,
+            y_train,
+            eval_set=[(X_val, y_val)],
+            **fit_params
+            if fit_params
+            else DefaultLGBMConfiguration["fit_params"],
         )
         importances = regressor.feature_importances_
         normalized_importances = importances / importances.sum()
@@ -100,8 +131,6 @@ def initialize_regressor(
         return ExtraTreesRegressor(**init_params)
     elif name == "LGBM":
         return LGBMRegressor(**init_params)
-    elif name == "XGB":
-        return XGBRegressor(**init_params)
     else:
         raise ValueError(
             f"Invalid tree method. Choose between: {RegressorName.__args__}"
@@ -115,6 +144,7 @@ if __name__ == "__main__":
     X, y = make_regression(n_samples=100, n_features=2, noise=0.1)
 
     rf_regressor = RandomForestRegressor()
+    print(rf_regressor)
     rf_regressor.fit(X, y)
     print(rf_regressor.feature_importances)
 
@@ -128,13 +158,6 @@ if __name__ == "__main__":
         y,
     )
     print(lgbm_regressor.feature_importances)
-
-    xgb_regressor = XGBRegressor()
-    xgb_regressor.fit(
-        X,
-        y,
-    )
-    print(xgb_regressor.feature_importances)
     try:
         failed_regressor = initialize_regressor("DUMMY", {})
     except ValueError as e:
